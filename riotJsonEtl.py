a6cf83037dc61cd92297b787ba00dcdc3e7aab54
vcarrel <54559035+vcarrel@users.noreply.github.com>
2019-11-06 16:06:17 -1000

    NO SINGLE VALUE; WORKING ON SECOND TABLE

--- riotJsonEtl.py
+++ riotJsonEtl.py
@@ -54,45 +54,14 @@ for item in partIds: - place info row by row
 # Notice that one of the keys is: participantIdentities
 # Use the key "participantIdentnties" to extract/isolate its dict value/"payload"
 partIds = jsd['participantIdentities']
-'''
+
 #expanded upon above command to include other keys
-seaId = jsd['seasonId']
-df_seaId = pd.DataFrame( {} )
-#not list, no keys print(seaId[0].keys())
-queId = jsd['queueId']
-df_queId = pd.DataFrame( {} )
-#not list, no keys print(queId[0].keys())
-gameId = jsd['gameId']
-df_gameId = pd.DataFrame( {} )
-#not list, no keys print(gameId[0].keys())
-gVers = jsd['gameVersion']
-df_gVers = pd.DataFrame( {} )
-#not list, no keys print(gVers[0].keys())
-platId = jsd['platformId']
-df_platId = pd.DataFrame( {} )
-#not list, no keys print(platId[0].keys())
-gMode = jsd['gameMode']
-df_gMode = pd.DataFrame( {} )
-#not list, no keys print(gMode[0].keys())
-mapId = jsd['mapId']
-df_mapId = pd.DataFrame( {} )
-print(mapId[0].keys())
-gType = jsd['gameType']
-df_gType = pd.DataFrame( {} )
-print(gType[0].keys())
 teams = jsd['teams']
+df_bans = pd.DataFrame( {'Team':0, 'pickTurn':0, 'championID':0} )
 df_teams = pd.DataFrame( {} )
-print(teams[0].keys())
-partics = jsd['participants']
-df_partics = pd.DataFrame( {} )
-print(partics[0].keys())
-gDura = jsd['gameDuration']
-df_gDura = pd.DataFrame( {} )
-print(gDura[0].keys())
-gCrea = jsd['gameCreation']
-df_gCrea = pd.DataFrame( {} )
-print(gCrea[0].keys())
-'''
+
+#print(teams[0].keys())
+
 #type(partIds)
 # list
 
@@ -118,19 +87,77 @@ for item in partIds:
 #    print('\t', item['player'], '\n')
     df_players = df_players.append(pd.DataFrame( item['player'], index=[item['participantId']] ) )
 
-#print('RESULT:')
+#print(type(teams[0]['firstDragon']))
+#print(teams)
+
+for item in teams:
+    df_teams = df_teams.append(pd.DataFrame(item))
+#nest bans w/ columns (team, picturn, champid)
+for item in df_teams['firstDragon']:
+    if item == True:
+        df_teams['firstDragon'] = 1
+#        print (df_teams['firstDragon'])
+    elif item == False:
+        df_teams['firstDragon'] = 0
+#        print(df_teams['firstDragon'])
+
+for item in df_teams['firstInhibitor']:
+    if item == True:
+        df_teams['firstInhibitor'] = 1
+#        print (df_teams['firstInhibitor'])
+    elif item == False:
+        df_teams['firstInhibitor'] = 0
+#        print(df_teams['firstInhibitor'])
+
+for item in df_teams['firstRiftHerald']:
+    if item == True:
+        df_teams['firstRiftHerald'] = 1
+#        print (df_teams['firstRiftHerald'])
+    elif item == False:
+        df_teams['firstRiftHerald'] = 0
+#        print(df_teams['firstRiftHerald'])
+
+for item in df_teams['firstBaron']:
+    if item == True:
+        df_teams['firstBaron'] = 1
+#        print (df_teams['firstBaron'])
+    elif item == False:
+        df_teams['firstBaron'] = 0
+#        print(df_teams['firstBaron'])
+
+for item in df_teams['firstBlood']:
+    if item == True:
+        df_teams['firstBlood'] = 1
+#        print (df_teams['firstBlood'])
+    elif item == False:
+        df_teams['firstBlood'] = 0
+#        print(df_teams['firstBlood'])
+
+for item in df_teams['firstTower']:
+    if item == True:
+        df_teams['firstTower'] = 1
+#        print (df_teams['firstTower'])
+    elif item == False:
+        df_teams['firstTower'] = 0
+#        print(df_teams['firstTower'])
+
+
+#        teams['firstDragon'] = 1
+#print(teams[teamId])
+
+#print('RESULT:'firstDragon)
 #print(df_players)
 
 #print('Printing the DataFrame column-by-column:')
 
-#for colname in df_players.columns:
+#for colname in df_players.columns,item[]:
 #    print('column:', colname)
 #    print(df_players[colname])
 #    print()
 
 
 #partIds = jsd['participantIdentities']
-#create sqlite3 database/table
+#create sqlite3 database/tablprint(partIds[0].keyprint(partIds[0].keys())s())e
 conn = sqlite3.connect('etlproject.db')
 c = conn.cursor()
 
@@ -145,3 +172,18 @@ df_players.to_sql('participantIdentities', conn, if_exists='replace', index=True
 
 #for row in c.fetchall():
 #    print (row)
+#donn = sqlite3.connect('etlproject.db')
+#d = donn.cursor()
+#c.execute("CREATE TABLE IF NOT EXISTS teams (firstDragon text, bans text, firstInhibitor text, win text, firstRiftHerald text, firstBaron text, baronKills integer, riftHeraldKills integer, firstBlood text, teamId integer, firstTower text, vilemawKills integer, inhibitorKills integer, towerKills integer, dominionVictoryScore integer, dragonKills integer)")
+c.execute("CREATE TABLE IF NOT EXISTS teams (firstDragon integer, bans text, firstInhibitor integer, win text, firstRiftHerald integer, firstBaron integer, baronKills integer, riftHeraldKills integer, firstBlood integer, teamId integer, firstTower integer, vilemawKills integer, inhibitorKills integer, towerKills integer, dominionVictoryScore integer, dragonKills integer)")
+
+conn.commit()
+
+#for item in df_teams:
+#    conn.execute("INSERT INTO teams", teams[item])
+
+#df_teams.to_sql('teams',conn,if_exists='replace',index=True)
+#print (df_teams.firstDragon)
+for item in df_teams:
+    print(item)
+    print(df_teams[item])
